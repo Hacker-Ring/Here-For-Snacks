8265127ea7b24e55761bbe4647b0d65eafb3a596
nishankx <nishankashyapofficial@gmail.com>
2025-09-27 19:55:31 +0530

    Clearing

--- stride/src/main/java/com/hfs/stride/model/RepoInfo.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.hfs.stride.model;
-
-import lombok.Data;
-
-@Data
-public class RepoInfo {
-    private String url;
-    private String name;
-}
--- stride/src/main/java/com/hfs/stride/repo/RepoResultRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.hfs.stride.repo;
-
-import com.stride.stride.model.RepoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepoResultRepository extends JpaRepository<RepoEntity, Long> {
-    RepoEntity findbyUrl(String url);
-}
